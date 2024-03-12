40d4d2b4f5e840b402a52fbb9cb9e2c779d03a92
thundich <144551058+thundich@users.noreply.github.com>
2024-03-12 19:09:20 -0400

    Add files via upload

---
+++ SQL/Null Queries.sql
@@ -0,0 +1,20 @@
+Select * from public."Market Data"
+ where state is null or 
+ state_code is null or 
+ property_type is null or 
+ property_type_id is null or 
+ median_sale_price is null or 
+ median_list_price is null or 
+ median_ppsf is null or 
+ median_list_ppsf is null or 
+ homes_sold is null or
+ pending_sales is null or 
+ new_listings is null or
+ inventory is null or
+ months_of_supply is null or
+ avg_sale_to_list is null or
+ sold_above_list is null or
+ parent_metro_region is null or
+ year is null or
+ month is null or
+ rei_id is null 
